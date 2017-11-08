22b12e60d336188fc509f9f0c56165e3cfd7157f
Kevin J Donohue <kevinjdonohue@yahoo.com>
2017-11-07 21:04:10 -0800

    adding airbnb package to provide eslint rules, fixed initial eslinting rule violations

--- tools/srcServer.js
+++ tools/srcServer.js
@@ -1,8 +1,9 @@
 import express from 'express';
 import webpack from 'webpack';
 import path from 'path';
-import config from '../webpack.config.dev';
 import open from 'open';
+
+import config from '../webpack.config.dev';
 
 /* eslint-disable no-console */
 
@@ -12,16 +13,16 @@ const compiler = webpack(config);
 
 app.use(require('webpack-dev-middleware')(compiler, {
   noInfo: true,
-  publicPath: config.output.publicPath
+  publicPath: config.output.publicPath,
 }));
 
 app.use(require('webpack-hot-middleware')(compiler));
 
-app.get('*', function(req, res) {
-  res.sendFile(path.join( __dirname, '../src/index.html'));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../src/index.html'));
 });
 
-app.listen(port, function(err) {
+app.listen(port, (err) => {
   if (err) {
     console.log(err);
   } else {
---
+++ tools/startMessage.js
@@ -0,0 +1,5 @@
+/* eslint-disable no-console, no-unused-vars */
+
+import colors from 'colors';
+
+console.log('Starting app in dev mode...'.green);
