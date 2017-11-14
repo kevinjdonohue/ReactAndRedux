e76b44836829c74ddcfd571c74ceb87ab43e830d
Kevin J Donohue <kevinjdonohue@yahoo.com>
2017-11-13 20:50:19 -0800

    Added annotations in comments to outline the 5 parts of a Container Component

--- src/components/course/CoursesPage.jsx
+++ src/components/course/CoursesPage.jsx
@@ -3,11 +3,11 @@ import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import * as courseActions from '../../actions/courseActions';
 
-class CoursesPage extends React.Component {
-  static courseRow(course, index) {
-    return <div key={index}>{course.title}</div>;
-  }
+// 5 Parts of a Container Component in React & Redux -- see below
 
+class CoursesPage extends React.Component {
+  // (1) Constructor:  initialize state, bind functions
+  /* eslint-disable react/sort-comp */
   constructor(props, context) {
     super(props, context);
 
@@ -19,6 +19,7 @@ class CoursesPage extends React.Component {
     this.onClickSave = this.onClickSave.bind(this);
   }
 
+  // (2) Child Functions:
   onTitleChange(event) {
     const { course } = this.state;
     course.title = event.target.value;
@@ -29,6 +30,12 @@ class CoursesPage extends React.Component {
     this.props.actions.createCourse(this.state.course);
   }
 
+  static courseRow(course, index) {
+    return <div key={index}>{course.title}</div>;
+  }
+
+  // (3) Render Method:
+  // NOTE:  This markup should be moved into a separate, presentation component; for demo purposes
   render() {
     // debugger;
     return (
@@ -43,12 +50,15 @@ class CoursesPage extends React.Component {
   }
 }
 
+// (4) PropTypes:  PropType validation
 /* eslint-disable react/forbid-prop-types */
 CoursesPage.propTypes = {
   courses: PropTypes.array.isRequired,
   actions: PropTypes.object.isRequired,
 };
 
+// (5) Redux Related Functions:
+// connect, bindActionCreators, mapStateToProps, mapDispatchToProps, etc.
 function mapStateToProps(state) {
   // debugger;
   return {
