2720c2005ec2ab8fc15578c3230beb5759fdb556
Kevin J Donohue <kevin.donohue@alaskaair.com>
2017-12-08 08:43:24 -0800

    updating sinon example

--- src/sinon-examples/Course.test.js
+++ src/sinon-examples/Course.test.js
@@ -1,23 +1,17 @@
 import expect from 'expect';
 import sinon from 'sinon';
-import sinonTestFactory from 'sinon-test';
 import Course from './Course';
 
-const sinonTest = sinonTestFactory(sinon);
-
 describe('Course', () => {
-  it(
-    'should stub doSomething',
-    sinonTest(function wrapper() {
-      // arrange
-      const stub = this.stub(Course, 'doSomething').callsFake(() => 'fakeDoSomething');
+  it('should stub doSomething', () => {
+    // arrange
+    const stub = this.stub(Course, 'doSomething').callsFake(() => 'fakeDoSomething');
 
-      // act
-      const result = Course.doSomethingElse();
+    // act
+    const result = Course.doSomethingElse();
 
-      // assert
-      expect(result).toBe('fakeDoSomething');
-      sinon.assert.calledOnce(stub);
-    }),
-  );
+    // assert
+    expect(result).toBe('fakeDoSomething');
+    sinon.assert.calledOnce(stub);
+  });
 });
